e75dd3ca9642d10dcae9a236aa02deafadaa3b38
Munir Hossain <60384012+mh-munir@users.noreply.github.com>
2024-10-13 19:36:46 +0600

    added service section

--- src/components/medicare/Medicare.jsx
+++ src/components/medicare/Medicare.jsx
@@ -1,4 +1,5 @@
 import {Playfair_Display} from 'next/font/google';
+import Image from 'next/image';
 const playfair = Playfair_Display({
     subsets: ['latin'],
     display: 'swap',
@@ -7,11 +8,9 @@ const playfair = Playfair_Display({
 const Medicare = ({MedicarClass, MedicareTitle, MedicareDes, MedicarIcon}) => {
   return (
     <div className={MedicarClass}>
-        <span>
-           {MedicarIcon}
-        </span>
+        <Image src={MedicarIcon} alt='Your Medicar life' />
         <div>
-            <h3 className={`text-2xl font-semibold text-gray-900 mb-3 ${playfair.className}`}>{MedicareTitle}</h3>
+            <h3 className={`text-2xl font-medium text-gray-700 mb-3 ${playfair.className}`}>{MedicareTitle}</h3>
             <p className="text-lg font-normal text-gray-900">{MedicareDes}</p>
         </div>
     </div>
---
+++ src/demodata/DemoData.js
@@ -0,0 +1,60 @@
+import Icon1 from "@/img/icon1.svg";
+import Icon2 from "@/img/icon2.svg";
+import Icon3 from "@/img/icon3.svg";
+import ServiceImg1 from '@/img/services-education.jpg';
+import ServiceImg2 from '@/img/services-discussion.jpg';
+import ServiceImg3 from '@/img/services-enrollment.jpg';
+import ServiceImg4 from '@/img/services-insurance-review.jpg';
+import ServiceImg5 from '@/img/services-renewal.jpg';
+
+const MediCareContents =[
+    {
+        icon: Icon1,
+        title: "Personalized Guidance",
+        des: "We focus on individualized approaches that build trust, empowering you to make informed decisions tailored to your health needs and budget.",
+    },
+
+    {
+        icon: Icon2,
+        title: "Comprehensive Medicare Education",
+        des: "We draw from deep industry expertise to specialize in Medicare, providing you with handpicked healthcare resources and knowledge such as free seminars and in-depth articles.",
+    },
+    {
+        icon:Icon3,
+        title: "Long-term Partnership",
+        des: "We foster lasting relationships, committed to your well-being from the start of your Medicare journey to assisting you as your healthcare needs change over time.",
+    }
+]
+
+export {MediCareContents};
+
+
+const ServiceContents =[
+    {
+        img: ServiceImg1,
+        alt: "Education",
+        title: "Education" 
+    },
+    {
+        img: ServiceImg2,
+        alt: "Discussion",
+        title: "Discussion" 
+    },
+    {
+        img: ServiceImg3,
+        alt: "Enrollment",
+        title: "Enrollment" 
+    },
+    {
+        img: ServiceImg4,
+        alt: "Insurance Review",
+        title: "Insurance Review" 
+    },
+    {
+        img: ServiceImg5,
+        alt: "Renewal",
+        title: "Renewal" 
+    },
+]
+
+export {ServiceContents};
